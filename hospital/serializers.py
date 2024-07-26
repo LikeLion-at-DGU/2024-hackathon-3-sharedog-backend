b115bc20e004ca3ea29ae891e769fa465353b863
che-hyeon <ghkdcogus03@naver.com>
2024-07-26 12:56:10 +0900

    Feat: Add dog, reservation

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -1,10 +1,49 @@
 from rest_framework import serializers
 from .models import *
 
-class HospitalSerializer(serializers.Modelserializer):
+class HospitalSerializer(serializers.ModelSerializer):
 
+    reservation = serializers.SerializerMethodField(read_only=True)
+    def get_reservation(self, instance):
+        serializer = ReservationSerializer(instance.reservations, many=True)
+        return serializer.data
+    
     class Meta:
         model = Hospital
         fields = '__all__'
+        read_only_fields = [
+            'id',
+            'created_at',
+            'updated_at',
+        ]
 
-        
+class DogSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+    def get_user(self, instance):
+        return instance.user.username
+
+    class Meta:
+        model = Dog
+        fields = '__all__'
+        read_only_fields = [
+            'id',
+            'user',
+            'created_at',
+            'updated_at',
+        ]
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    hospital = serializers.SerializerMethodField(read_only=True)
+    def get_hospital(self, instance):
+        hospital = instance.hospital
+        return hospital.name
+    
+    dog = serializers.SerializerMethodField(read_only=True)
+    def get_dog(self, instance):
+        return instance.dog.dog_name
+    
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+        read_only_fields = ['hospital', 'dog']
--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import *
+from rest_framework import viewsets, mixins
+from .serializers import HospitalSerializer, DogSerializer, ReservationSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HospitalViewSet(viewsets.ModelViewSet):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
+
+    # action을 이용해서 특정 지역에 해당하는 filtering
+
+class DogViewSet(viewsets.ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+    def list(self, request, user_id=None):
+        user = self.request.user
+        queryset = self.filter_queryset(self.get_queryset().filter(user=user))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+class ReservationViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+class HospitalReservationVieSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+    def list(self, request, hospital_id=None):
+        hospital = get_object_or_404(Hospital, id=hospital_id)
+        queryset = self.filter_queryset(self.get_queryset().filter(hospital=hospital))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request, hospital_id=None):
+        hospital = get_object_or_404(Hospital, id=hospital_id)
+        user = self.request.user
+        dog = Dog.objects.filter(user=user).first()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(hospital=hospital, dog=dog)
+        return Response(serializer.data)
