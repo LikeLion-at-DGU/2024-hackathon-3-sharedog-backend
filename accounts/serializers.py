4e0bfbe4690c16e3007d2a9c7c80ae8b1a962577
jjii-nu <tonywoo616@gmail.com>
2024-07-29 11:57:42 +0900

    Fix:profile수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,3 +6,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
+class DogProfileSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = DogProfile
+        fields = '__all__'
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import viewsets
 
-from .models import Profile
-from .serializers import ProfileSerializer
+from .models import Profile, DogProfile
+from .serializers import ProfileSerializer, DogProfileSerializer
 
 from django.shortcuts import get_list_or_404
 
@@ -15,9 +15,24 @@ class ProfileViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)  # 오타 수정
+        self.perform_create(serializer) 
 
         profile = serializer.instance
+        return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class DogProfileViewSet(viewsets.ModelViewSet):
+    queryset = DogProfile.objects.all()
+    serializer_class = DogProfileSerializer
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer) 
+
+        dogprofile = serializer.instance
         return Response(serializer.data)
 
     def perform_create(self, serializer):
