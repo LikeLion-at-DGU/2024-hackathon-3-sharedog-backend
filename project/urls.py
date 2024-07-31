b25068b2d8fd36c920f2bff4638989b85c1bb6f5
che-hyeon <ghkdcogus03@naver.com>
2024-08-01 00:08:54 +0900

    Fix: connect image url

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/', include('main.urls')),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('api/hospital/', include('hospital.urls')),
     path('api/users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
