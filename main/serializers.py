aba9fcf60910399019e2feb9edab90cad5596b5f
jjii-nu <tonywoo616@gmail.com>
2024-08-04 13:20:28 +0900

    Fix:total_score추가

--- main/serializers.py
+++ main/serializers.py
@@ -62,9 +62,54 @@ class TotaltestSerializer(serializers.ModelSerializer):
     is_vaccinated = VaccinetestSerializer()
     has_disease = DiseasetestSerializer()
     writer = serializers.SerializerMethodField(read_only=True)
+    score = serializers.SerializerMethodField()
+    can_blood = serializers.SerializerMethodField()
+
     def get_writer(self, instance):
         writer = instance.writer
         return writer.username
+
+    def get_score(self, instance):
+        score = 0
+
+        # Size에 따른 점수 부여
+        if instance.size.size == '소형견':
+            score += 0
+        elif instance.size.size == '대형견':
+            score += 10
+
+        # Age에 따른 점수 부여
+        if instance.age_group.age_group == '~18M':
+            score += 0
+        elif instance.age_group.age_group == '18M~8Y':
+            score += 5
+        elif instance.age_group.age_group == '9Y~':
+            score += 10
+
+        # Weight에 따른 점수 부여
+        if instance.weight_group.weight_group == '~20KG':
+            score += 0
+        elif instance.weight_group.weight_group == '20KG~':
+            score += 10
+
+        # Vaccination에 따른 점수 부여
+        if instance.is_vaccinated.is_vaccinated == '접종':
+            score += 10
+        elif instance.is_vaccinated.is_vaccinated == '미접종':
+            score -= 0
+
+        # Disease에 따른 점수 부여
+        if instance.has_disease.has_disease == '네':
+            score -= 0
+        elif instance.has_disease.has_disease == '아니요':
+            score += 10
+
+        return score
+    
+    def get_can_blood(self, instance):
+        score = self.get_score(instance)
+        return "헌혈 가능" if score >= 30 else "헌혈 불가"
+    
     class Meta:
         model = Totaltest
         fields = '__all__'
