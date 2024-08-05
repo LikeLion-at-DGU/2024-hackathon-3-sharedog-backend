aeec0da9f31746617330fcce66b1f74b062c97dd
che-hyeon <ghkdcogus03@naver.com>
2024-08-06 00:42:46 +0900

    Fix: media upload url 수정

--- hospital/urls.py
+++ hospital/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('', include(default_router.urls)),
     path('', include(reservation_router.urls)),
     path('home/<int:hospital_id>/', include(hospital_reservation_router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
